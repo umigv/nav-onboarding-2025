2f03093f552bbd024f94be2c0cfed7e5b5dfe131
Ryan Liao <ryan.liao0305@gmail.com>
2025-04-21 11:45:20 -0400

    Add clipboard copying to script

--- environment/generate-ssh.sh
+++ environment/generate-ssh.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-ssh-keygen -q -t ed25519 -N '' -f ~/.ssh/id_ed25519
+KEY_PATH="$HOME/.ssh/id_ed25519"
+
+ssh-keygen -q -t ed25519 -N '' -f $KEY_PATH
 
 if [ $? -eq 0 ]; then
-    echo "Your SSH Public Key (copy the entire line with ctrl+shift+c):"
-    cat ~/.ssh/id_ed25519.pub
+    echo "SSH key generated and copied to clipboard"
 else
-    echo "Did not generate a new SSH key"
-    echo "Your existing SSH Public Key (copy the entire line with ctrl+shift+c):"
-    cat ~/.ssh/id_ed25519.pub
+    echo "Did not generate a new SSH key, existing key is copied to clipboard"
 fi
+
+xclip -selection clipboard < "$KEY_PATH.pub"
+echo "Your SSH Public Key:"
+cat $KEY_PATH.pub
